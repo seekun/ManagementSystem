29d6c39767ba305f63524650c18e79c6276a0961
seekun <tmsshikun@gmail.com>
2018-07-11 01:00:33 -0700

    paper detail

--- django_auth_example/web/urls.py
+++ django_auth_example/web/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^paper/', views.paper, name='paper'),
     url(r'^search/', views.search, name='search'),
     url(r'^profile/', views.profile, name='profile'),
+    url(r'^detail/(?P<page_num>.*?)$', views.detail, name='detail'),
     # url(r'^mypaper', views.mypaper, name='mypaper'),
 ]
--- django_auth_example/web/views.py
+++ django_auth_example/web/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, Http404, redirect, HttpResponse
 from .models import ArtiInFo
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 # from django_auth_example.users.models import User
 
 
 def paper(request, query=None):
-
-    print(query)
-    limit = 5
     queryset_list = ArtiInFo.objects.all()
     # query = request.GET.get("q")
     find_paper = []
     for i in queryset_list:
         if query:
-            if query in i.author:
-                find_paper.append(i)
+            for author in i.authors:
+                if query in author:
+                    find_paper.append(i)
 
     context = {}
     if query:
@@ -24,7 +24,6 @@ def paper(request, query=None):
             if query:
                 if query in i.title:
                     find.append(i)
-
 
         # page_robot = Paginator(find, 5)
         # page = request.GET.get('page')
@@ -87,9 +86,7 @@ def profile(request):
         return paper(request, query)
     # 开始的时候没有加return,直接调用paper视图函数,结果没有返回结果.
 
-
     return render(request, 'web/user_profile.html')
-
 
 # def mypaper(request):
 #     context = {}
@@ -120,4 +117,17 @@ def profile(request):
 #         return render(request, 'web/index.html', context)
 #     else:
 #         return render(request, 'web/index.html')
+
+
+def detail(request, page_num):
+    context = {}
+    paper_detail = None
+    all_papers = ArtiInFo.objects.all()
+    for i in all_papers:
+        if page_num == str(i['id']):
+            paper_detail = i
+    p = paper_detail
+    context['p'] = p
+
+    return render(request, 'web/detail.html', context)
 
