a12edd999e241dca7e881fd2ba7af9a7e2fe36cc
seekun <tmsshikun@gmail.com>
2018-07-12 19:19:22 -0700

    prepare for delete database

--- django_auth_example/users/models.py
+++ django_auth_example/users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import JSONField
+from jsonfield import JSONField
 import json
 
 
 class User(AbstractUser):
     user_number = models.CharField('工号', max_length=20, blank=True)
     chinese_name = models.CharField('中文名', max_length=20, blank=True)
-    english_name = JSONField(blank=True, null=True)
+    english_name = JSONField(null=True, blank=True, default=None)
     # list字段,可以有多个english_name
     department = models.CharField('所在院系名称', max_length=20, blank=True)
 
